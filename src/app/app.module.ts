c37888b9f0febb11c0cf88b575652420fc374ac7
Muhammad Talha <bsem-f18-206@superior.edu.pk>
2022-04-04 12:16:03 +0500

    Intial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonModule } from 'primeng/button';
+import { ImageModule } from 'primeng/image';
+import { CardModule } from 'primeng/card';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+import { FormsModule } from '@angular/forms';
+import {RatingModule} from 'primeng/rating';
+import {InputTextModule} from 'primeng/inputtext';
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    ImageModule,
+    CardModule,
+    FontAwesomeModule,
+    FormsModule,
+    RatingModule,
+    InputTextModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+}
